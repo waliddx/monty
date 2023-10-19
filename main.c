28f3e6f4445ab2e44b9c8c7b70f2393f55200554
waliddx <wagzouly@gmail.com>
2023-10-19 06:54:09 -0700

    x

--- main.c
+++ main.c
@@ -1,4 +1,7 @@
 #include "monty.h"
+#include <stdio.h>
+/* Declaring getline function */
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
@@ -20,26 +23,33 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	bus.file = file;
+
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+
 	while (read_line > 0)
 	{
 		content = NULL;
 		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
+
 		if (read_line > 0)
 		{
 			execute(content, &stack, counter, file);
 		}
 		free(content);
 	}
+
 	free_stack(stack);
 	fclose(file);
-return (0);
+
+	return (0);
 }
+
